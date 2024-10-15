f97b34e5a2dd5e5a7786775a0e70ca83a77f9951
loll <notEmail@tt.com>
2024-10-15 17:47:07 +0300

    fixed wait time

--- src/main/java/pages/Product.java
+++ src/main/java/pages/Product.java
@@ -74,7 +74,7 @@ public class Product extends HomePage {
     public Product chooseColor(String color) {
         if (ElementActions.isElementDisplayed(driver, productColorsCount)) {
             try {
-                ElementActions.waitExplicitly(driver, 2, productColors(color), Waits.VISIBLE.toString());
+                ElementActions.waitExplicitly(driver, 8, productColors(color), Waits.VISIBLE.toString());
                 ElementActions.click(driver, productColors(color));
                 CustomLogger.logger.info("chose color: " + color);
             } catch (Exception e) {
